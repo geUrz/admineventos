dd649293b2b54f891732d6f69a6cca7e8e3f7a63
geUrz <gerardourzua20@gmail.com>
2024-10-01 16:15:52 -0700

    start4

--- src/libs/onesignal.js
+++ src/libs/onesignal.js
@@ -1,46 +1,15 @@
 export const initializeOneSignal = () => {
-  window.OneSignal = window.OneSignal || [];
-  OneSignal.push(function() {
-    OneSignal.init({
-      appId: process.env.NEXT_PUBLIC_ONESIGNAL_APP_ID,
-    });
-
-    OneSignal.push(() => {
-      OneSignal.registerForPushNotifications(); // Registra al usuario para notificaciones
-
-      OneSignal.getUserId().then((playerId) => {
-        if (!playerId) {
-          console.error('Player ID no disponible. Asegúrate de que el usuario haya aceptado las notificaciones.');
-          return;
-        }
-
-        const userId = getCookie('userId'); // Asegúrate de que 'userId' esté en las cookies
-        if (userId) {
-          fetch('/api/savePlayerId', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ playerId, userId }),
-          })
-          .then(response => response.json())
-          .then(data => {
-            console.log(data.message);
-          })
-          .catch(error => {
-            console.error('Error al enviar Player ID:', error);
-          });
-        } else {
-          console.error('User ID no disponible en las cookies');
-        }
+    if (typeof window !== 'undefined') {
+      window.OneSignal = window.OneSignal || [];
+      OneSignal.push(function() {
+        OneSignal.init({
+          appId: process.env.NEXT_PUBLIC_ONESIGNAL_APP_ID,
+          notifyButton: {
+            enable: true, 
+          },
+          allowLocalhostAsSecureOrigin: true, 
+        });
       });
-    });
-  });
-};
-
-// Función para obtener cookies
-const getCookie = (name) => {
-  const value = `; ${document.cookie}`;
-  const parts = value.split(`; ${name}=`);
-  if (parts.length === 2) return parts.pop().split(';').shift();
-};
+    }
+  };
+  
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,24 +4,12 @@ import { initializeOneSignal } from '@/libs/onesignal'
 import { useEffect } from 'react'
 import '@/styles/globals.css'
 
-const getCookie = (name) => {
-  const value = `; ${document.cookie}`;
-  const parts = value.split(`; ${name}=`);
-  if (parts.length === 2) return parts.pop().split(';').shift();
-}
-
 export default function App(props) {
 
   const { Component, pageProps } = props
 
   useEffect(() => {
-    const userId = getCookie('userId')
-    
-    if (userId) {
-      initializeOneSignal()
-    } else {
-      console.log('User ID no disponible. Asegúrate de hacer login primero.');
-    }
+    initializeOneSignal()
   }, [])
 
   return(
